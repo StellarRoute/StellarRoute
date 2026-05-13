7df34307966a57752177170545d7c800a6f5de95
熊晖 <xionghui@xionghuideMacBook-Air.local>
2026-05-14 02:46:35 +0800

    feat: unify swap view states

--- frontend/components/shared/index.ts
+++ frontend/components/shared/index.ts
@@ -10,3 +10,5 @@ export { QuoteCard } from './QuoteCard';
 export { RouteRow } from './RouteRow';
 export { SlippageControl } from './SlippageControl';
 export { ExplorerLink } from './ExplorerLink';
+export { SwapViewState, ViewState } from './ViewState';
+export type { SwapViewStateKind, ViewStateVariant } from './ViewState';
